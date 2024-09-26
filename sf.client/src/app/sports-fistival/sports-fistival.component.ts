2941830f75c575219435117e69076613e7166516
Amal <amal.abdalla@ovgu.de>
2024-09-26 17:26:28 +0200

    'added'

---
+++ sf.client/src/app/sports-fistival/sports-fistival.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sports-fistival',
+  templateUrl: './sports-fistival.component.html',
+  styleUrl: './sports-fistival.component.css'
+})
+export class SportsFistivalComponent {
+
+}
